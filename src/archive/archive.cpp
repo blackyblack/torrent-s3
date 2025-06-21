0227651217551432d2fb35714ecd0f000069c8dc
Andrey Lekar <andrey_lekar@adoriasoft.com>
2025-06-21 18:12:06 +0200

    Minor fixes

--- src/archive/archive.cpp
+++ src/archive/archive.cpp
@@ -132,7 +132,7 @@ std::optional<std::string> zip_file(std::filesystem::path source_path, std::file
     }
     std::filesystem::create_directories(std::filesystem::u8path(dest_path.parent_path().string()));
 
-    auto fd = open(dest_file.c_str(), O_RDWR | _O_CREAT);
+    auto fd = open(dest_file.c_str(), O_RDWR | _O_CREAT | O_TRUNC);
     if (fd < 0) {
         return std::string("Failed to write file \"") + dest_file + "\"";
     }
--- src/archive/archive.hpp
+++ src/archive/archive.hpp
@@ -15,4 +15,4 @@ bool is_packed(std::filesystem::path file_name);
 
 std::variant<std::vector<file_unpack_info_t>, std::string> unpack_file(std::filesystem::path file_name, std::filesystem::path output_directory);
 
-std::optional<std::string> zip_file(std::filesystem::path file_name, std::filesystem::path output_directory);
+std::optional<std::string> zip_file(std::filesystem::path source_path, std::filesystem::path dest_path);
