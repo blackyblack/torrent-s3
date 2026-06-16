63d14cd2a3d38f5578b8be4450e43d885ecb21fc
blackyblack <johnmalkovich@mail.ru>
2026-06-16 18:50:03 +0200

    Fix bucket check (#9)
    * Fix bucket existence check
    
    * Support http url
    
    ---------
    
    Co-authored-by: Andrey Lekar <andrey_lekar@adoriasoft.com>

--- src/s3/s3.cpp
+++ src/s3/s3.cpp
@@ -16,6 +16,8 @@
 #define INITIAL_DELAY_SECONDS 5
 #define MAX_DELAY_SECONDS 60
 
+static minio::s3::BaseUrl make_base_url(const std::string &url);
+
 S3Uploader::S3Uploader(
     unsigned int thread_count_,
     const std::string &url_,
@@ -38,7 +40,7 @@ S3Uploader::S3Uploader(
         thread_count = TASKS_COUNT_DEFAULT;
     }
 
-    minio::s3::BaseUrl base_url(url);
+    minio::s3::BaseUrl base_url = make_base_url(url);
     provider = std::make_unique<minio::creds::StaticProvider>(access_key, secret_key);
     client = std::make_unique<minio::s3::Client>(base_url, provider.get());
 }
@@ -64,6 +66,18 @@ static std::string replace(std::string subject, const std::string& search, const
         pos += replace.length();
     }
     return subject;
+}
+
+static minio::s3::BaseUrl make_base_url(const std::string &url) {
+    const auto http_prefix = std::string("http://");
+    const auto https_prefix = std::string("https://");
+    if (url.find(http_prefix) == 0) {
+        return minio::s3::BaseUrl(url.substr(http_prefix.size()), false);
+    }
+    if (url.find(https_prefix) == 0) {
+        return minio::s3::BaseUrl(url.substr(https_prefix.size()), true);
+    }
+    return minio::s3::BaseUrl(url);
 }
 
 static std::optional<std::string> write_stream_s3(std::istream &stream, unsigned long content_size, minio::s3::Client &client, const std::string &bucket, const std::string &region, const std::filesystem::path &path) {
@@ -149,7 +163,7 @@ static std::variant<bool, std::string> exists_bucket_s3(minio::s3::Client &clien
     }
 
     std::string error = "Retry limit reached";
-    bool exists;
+    bool exists = false;
     const auto result = backoffxx::attempt(backoffxx::make_exponential(std::chrono::seconds(INITIAL_DELAY_SECONDS), RETRIES, std::chrono::seconds(MAX_DELAY_SECONDS)), [&] {
         const auto resp = client.BucketExists(args);
         if (!resp) {
@@ -160,9 +174,7 @@ static std::variant<bool, std::string> exists_bucket_s3(minio::s3::Client &clien
             error = resp.Error().String();
             return backoffxx::attempt_rc::hard_error;
         }
-        if (resp.exist) {
-            exists = true;
-        }
+        exists = resp.exist;
         return backoffxx::attempt_rc::success;
     });
 
@@ -171,6 +183,33 @@ static std::variant<bool, std::string> exists_bucket_s3(minio::s3::Client &clien
     }
 
     return exists;
+}
+
+static std::optional<std::string> create_bucket_s3(minio::s3::Client &client, const std::string &bucket, const std::string &region) {
+    minio::s3::MakeBucketArgs args;
+    args.bucket = bucket;
+    if (!region.empty()) {
+        args.region = region;
+    }
+
+    std::string error = "Retry limit reached";
+    const auto result = backoffxx::attempt(backoffxx::make_exponential(std::chrono::seconds(INITIAL_DELAY_SECONDS), RETRIES, std::chrono::seconds(MAX_DELAY_SECONDS)), [&] {
+        const auto resp = client.MakeBucket(args);
+        if (!resp) {
+            // throttling - make retry
+            if (resp.status_code == 429 || resp.status_code == 0) {
+                return backoffxx::attempt_rc::failure;
+            }
+            error = resp.Error().String();
+            return backoffxx::attempt_rc::hard_error;
+        }
+        return backoffxx::attempt_rc::success;
+    });
+
+    if (!result.ok()) {
+        return error;
+    }
+    return std::nullopt;
 }
 
 static std::filesystem::path filename_to_archived_path(std::filesystem::path file_name, std::string file_prefix) {
@@ -193,7 +232,7 @@ static void s3_upload_task(
 ) {
     fprintf(stdout, "Starting S3 upload task #%u\n", task_index + 1);
 
-    minio::s3::BaseUrl base_url(url);
+    minio::s3::BaseUrl base_url = make_base_url(url);
     minio::creds::StaticProvider provider(access_key, secret_key);
     minio::s3::Client client(base_url, &provider);
 
@@ -251,7 +290,10 @@ std::optional<std::string> S3Uploader::start() {
     }
     const auto exists = std::get<bool>(exists_variant);
     if (!exists) {
-        return std::string("Bucket \"") + bucket + std::string("\" does not exist");
+        const auto create_option = create_bucket_s3(*client, bucket, region);
+        if (create_option.has_value()) {
+            return std::string("Could not create bucket \"") + bucket + std::string("\". Error: ") + create_option.value();
+        }
     }
 
     std::string empty_file;
